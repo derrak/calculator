a70a7a8b3524cc56a502c5a71770f866719ad80d
Derrak Richard <derrak@gmail.com>
2022-06-02 15:12:40 -0700

    add function that prevents dialog box to appear before user submit
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,8 +19,11 @@ function divide(number1, number2) {
 // Everything below this line is ui logic:
 
 $(document).ready(function() {
-  const number1 = parseInt($("#add1").val());
-  const number2 = parseInt($("#add2").val());
-  const result = add(number1, number2);
-  alert(result);
+	$("form#add").submit(function(event) {
+		event.preventDefault();
+		const number1 = parseInt($("#add1").val());
+		const number2 = parseInt($("#add2").val());
+		const result = add(number1, number2);
+		alert(result);
+	});
 });
