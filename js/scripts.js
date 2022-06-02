b1f5202a88f8d09e1c77bba7925790f5b4f3824d
Derrak Richard <derrak@gmail.com>
2022-06-02 15:03:57 -0700

    adds jQuery and form fields to HTM
    Co-authored-by: Ashe Urban <ashe@goldentongue.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,26 @@
-function add(number1, number2){
-	return number1+number2; 
+// Business Logic:
+
+function add(number1, number2) {
+	return number1 + number2; 
 }
 
-function subtract(number1, number2){
-	return number11number2; 
+function subtract(number1, number2) {
+	return number1 - number2; 
 }
 
-function multiply(number1, number2){
-	return number1*number2; 
+function multiply(number1, number2) {
+	return number1 * number2; 
 }
 
-function divide(number1, number2){
-	return number1/number2; 
+function divide(number1, number2) {
+	return number1 / number2; 
 }
 
-const number1= parseInt(prompt("Enter a number:"));
-const number2= parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
+// Everything below this line is ui logic:
 
-alert(result);
+$(document).ready(function() {
+  const number1 = parseInt($("#add1").val());
+  const number2 = parseInt($("#add2").val());
+  const result = add(number1, number2);
+  alert(result);
+});
